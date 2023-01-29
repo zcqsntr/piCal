b6757f69fe0987d467e8b4a846e610e8332496f5
Neythen Treloar <zcsqntr@ucl.ac.uk>
2023-01-29 10:43:14 +0000

    added setup file

---
+++ piCal/setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+sudo apt-get install python3-dev python3-pillow -y
+sudo pip3 install python-dateutil
+pip3 install pytz
+
+# enable spi interface
+# reboot
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
